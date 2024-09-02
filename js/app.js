807641329295ada9f67ad81ea30f6f4d8b2b62d1
MuminovMuslimbek <muslimbekmuminov47@gmail.com>
2024-09-02 11:07:29 +0300

    firs commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 document.querySelector('.explore-button').addEventListener('click', function() {
     alert('Hello bro! What is needed for you');
 });
+document.addEventListener('mousemove', function(e) {
+    const cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+});
